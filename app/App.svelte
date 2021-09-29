bf5b05a69ea72e386a29b00e942f1aca2cf060c5
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-09-29 16:29:20 +0800

    Completed tasks in 2nd tab & let user restore task

--- app/App.svelte
+++ app/App.svelte
@@ -23,7 +23,11 @@
             
         </tabContentItem>
         <tabContentItem>
-            <label text='List of completed tasks for tracking'/>
+            <listView items={dones} on:itemTap={onDoneTap}>
+					<Template let:item>
+						<label text={item.name} textWrap='true' />
+					</Template>
+				</listView>
         </tabContentItem>
     </tabs>
 
@@ -82,4 +86,34 @@
         
         textFieldValue = ''
     }
+
+	 const onDoneTap = async (args) => {
+		let result = await action("What do you want to do with this task?", 
+			"Cancel", 
+			[
+				"Mark To Do", 
+				'Delete forever'
+			]
+		);
+
+		console.log({ result });
+
+		let item = dones[args.index];
+
+		switch (result) {
+			case 'Mark To Do':
+				todos = addToList(todos, item);
+				dones = removeFromList(dones, item);
+				break;
+			case 'Delete forever':
+				dones = removeFromList(dones, item);
+				break;
+		
+			case "Cancel" || undefined:
+				break;
+
+			default:
+				break;
+		}
+	 }
 </script>
